16071562920d6372a15ee842c9f0365bf81c3aa3
Christian Berndt <christian.berndt@liferay.com>
2024-10-14 20:16:38 +0200

    feat: refactor to async filter.

--- src/Autocomplete.js
+++ src/Autocomplete.js
@@ -1,114 +1,76 @@
 import React, { useState } from 'react';
 import ClayAutocomplete from '@clayui/autocomplete';
 import { FetchPolicy, NetworkStatus, useResource } from '@clayui/data-provider';
-import DropDown from '@clayui/drop-down';
-import {useDebounce} from '@clayui/shared';
 
-const Autocomplete = () => {
+export const Autocomplete = () => {
+	const [value, setValue] = useState('');
 
-    const LoadingWithDebounce = ({
-        loading,
-        networkStatus,
-        render,
-    }) => {
-        const debouncedLoadingChange = useDebounce(loading, 500);
-
-        if (networkStatus === 1 || debouncedLoadingChange) {
-            return <DropDown.Item className="disabled">Loading...</DropDown.Item>;
-        }
-
-        return render;
-    };
-
-    const [value, setValue] = useState('');
-    const [visible, setVisible] = useState(false);
     const [networkStatus, setNetworkStatus] = useState(
-        // const [networkStatus, setNetworkStatus] = useState<NetworkStatus>(
         NetworkStatus.Unused
     );
-    const { resource } = useResource({
-        fetchPolicy: FetchPolicy.CacheFirst,
-        link: 'https://rickandmortyapi.com/api/character',
-        onNetworkStatusChange: setNetworkStatus,
-        variables: { name: value },
-    });
+	const {resource} = useResource({
+        fetch: async (link) => {
 
-    const initialLoading = networkStatus === 1;
-    const loading = networkStatus < 4;
-    const error = networkStatus === 5;
+            let data = undefined;
 
-    return (
-        <div className="row">
-            <div className="col-md-5">
-                <div className="sheet">
-                    <div className="form-group">
-                        <label>Name</label>
-                        <ClayAutocomplete>
-                            <ClayAutocomplete.Input
-                                aria-label="Enter a name:"
-                                onChange={(event) =>
-                                    setValue(event.target.value)
-                                }
-                                onFocus={(event) => {
-                                    setVisible(true)
-                                }}
-                                onBlur={(event) => {
-                                    setVisible(false)
-                                }}
-                                value={value}
-                            />
+            data = await fetch(link,
+                {
+                headers: {}
+                }
+            ).then(response => response.json());
 
-                            <ClayAutocomplete.DropDown
-                                active={
-                                    (!!resource && !!value) || visible
-                                }
-                            >
-                                <DropDown.ItemList>
-                                    <LoadingWithDebounce
-                                        loading={loading}
-                                        networkStatus={networkStatus}
-                                        render={
-                                            <>
-                                                {(error ||
-                                                    (resource &&
-                                                        resource.error)) && (
-                                                        <DropDown.Item className="disabled">
-                                                            No Results Found
-                                                        </DropDown.Item>
-                                                    )}
-                                                {!error &&
-                                                    resource &&
-                                                    resource.results &&
-                                                    resource.results.map(
-                                                        (item) => (
-                                                            <ClayAutocomplete.Item
-                                                                key={item.id}
-                                                                match={value}
-                                                                onClick={() =>
-                                                                    setValue(
-                                                                        item.name
-                                                                    )
-                                                                }
-                                                                value={
-                                                                    item.name
-                                                                }
-                                                            />
-                                                        )
-                                                    )}
-                                            </>
-                                        }
-                                    />
-                                </DropDown.ItemList>
-                            </ClayAutocomplete.DropDown>
-                            {loading && <ClayAutocomplete.LoadingIndicator />}
-                        </ClayAutocomplete>
-                    </div>
-                </div>
-            </div>
-        </div>
+            return {
+                results: data.results,
+            };
 
-    )
 
-}
+        },
+		fetchPolicy: FetchPolicy.CacheFirst,
+		link: 'https://rickandmortyapi.com/api/character',
+		onNetworkStatusChange: setNetworkStatus,
+		variables: {name: value},
+	});
+
+	return (
+		<div className="row">
+			<div className="col-md-5">
+				<div className="sheet">
+					<div className="form-group">
+						<label
+							htmlFor="autocomplete-name"
+							id="autocomplete-name-label"
+						>
+							Name
+						</label>
+						<ClayAutocomplete
+							aria-labelledby="autocomplete-name-label"
+							id="autocomplete-name"
+							items={
+								(resource?.results) ?? []
+							}
+							loadingState={networkStatus}
+							messages={{
+								listCount: '{0} option available.',
+								listCountPlural: '{0} options available.',
+								loading: 'Loading...',
+								notFound: 'No results found',
+							}}
+							onChange={setValue}
+							onItemsChange={() => {}}
+							placeholder="Enter a name"
+							value={value}
+						>
+							{(item) => (
+								<ClayAutocomplete.Item key={item.id}>
+									{item.name}
+								</ClayAutocomplete.Item>
+							)}
+						</ClayAutocomplete>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Autocomplete;
